9efe58423eb945eb91709d9ade0b0c5eee5c3c85
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2021-12-06 21:29:07 +0000

    Add some feature to contract

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract waveContract {
+    uint256 totalWaves;
     constructor() {
         console.log("Hello BlockChain World");
     }
+
+    function wave() public{
+        totalWaves +=1;
+        console.log("%s has waved",msg.sender);
+    }
+
+    function getTotalWaves() public view returns(uint256){
+        console.log("We have %d total waves!",totalWaves);
+        return totalWaves;
+    }
+
 }
